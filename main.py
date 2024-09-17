d44fe50954bab98d35e33fd7e901af4e1c64f7d0
beaudrytinklerl <beaudrytinklerl@educ.college-montreal.qc.ca>
2024-09-17 14:52:42 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+"""
+nom : Lucas B. Tinkler
+Groupe : 401
+contexte : compte et renvoie le nombre de mots dans une chaine de caractère
+"""
+
+chaine_de_caractere = "  j'aime les carottes et les pistaches yummmmmmmmmmmmmmmmmmmmmmm "
+print(chaine_de_caractere.split())
+print(len(chaine_de_caractere.split()))
